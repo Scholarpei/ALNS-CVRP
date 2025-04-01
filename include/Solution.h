67928e250e2d160f7470835013561e1bc3a70ff2
T-TRVL <107686928@qq.com>
2025-04-01 20:59:15 +0800

    在solution类添加了update函数用于更新路径和总路程，便于算子实现

--- include/Solution.h
+++ include/Solution.h
@@ -13,6 +13,7 @@ class Solution
     Solution(vector<int> nodes_seq, Model& model) : nodes_seq(nodes_seq){}
     pair<int, vector<vector<int>>> splitRoutes(Model& model);
     double evaluateSolution(Model& model);
+    void update(Model& model);
 
 };
 
